bce89e1531663ecdad7ce1091bdfa12475a64584
Murat Çelik <celikmurat1845@gmail.com>
2024-02-18 02:22:33 +0300

    feat: setup client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Chat from './pages/Chat';
+
+function App() {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <Chat />
+            </header>
+        </div>
+    );
+}
+
+export default App;
